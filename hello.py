cec6b5fdb7e6b17d090749c57aff5277b49bbba6
Eli <ethds24@gmail.com>
2017-10-13 19:30:08 -0400

    Added a binary version of Hello World!

--- hello.py
+++ hello.py
@@ -3,6 +3,7 @@ import random
 greetings = [
     "Hello, World!",
     "Hi, World!",
+    "01001000 01100101 01101100 01101100 01101111 00101100 00100000 01010111 01101111 01110010 01101100 01100100",
 ]
 
 print(random.choice(greetings))
