3c8cbd278675b9146ebbf07d1367258fb0820ad1
Helena <helenasheinonen@gmail.com>
2017-10-15 18:28:26 -0400

    added oopsie poopsie

--- hello.py
+++ hello.py
@@ -8,6 +8,7 @@ greetings = [
     "01001000 01100101 01101100 01101100 01101111 00101100 00100000 01010111 01101111 01110010 01101100 01100100",
     "안녕 얘들아",
     "I like cheese"
+    "oopsie poopsie"
 ]
 
 print(random.choice(greetings))
