f464d6f0045d6fa514c8ee5390bff5be6f8dfadb
Eli <ethds24@gmail.com>
2017-11-19 17:49:58 -0500

    Unity Simulation

---
+++ Simulation/robot_sim/Assets/robot_input.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class robot_input : MonoBehaviour {
+	
+    public float speed = 10f;
+    //public Transform robot;
+    //public float movetime;
+	// Use this for initialization
+	void Start () {
+		
+		
+	}
+	public void checkInput(){
+		Vector3 pos = transform.position;
+
+        if (Input.GetKey("w") )
+        {
+            
+            //pos.y += panSpeed * Time.deltaTime;
+            //Debug.Log(Time.deltaTime);
+            //moveUp(speed);
+            robot_move sn = gameObject.GetComponent<robot_move>();
+            sn.moveUp(speed);
+            
+        }
+        if (Input.GetKey("s"))
+        {
+            //pos.y -= panSpeed * Time.deltaTime;
+            robot_move sn = gameObject.GetComponent<robot_move>();
+            sn.moveDown(speed);
+        }
+        if (Input.GetKey("d"))
+        {
+            //pos.x += panSpeed * Time.deltaTime;
+            robot_move sn = gameObject.GetComponent<robot_move>();
+            sn.moveRight(speed);
+        }
+        if (Input.GetKey("a"))
+        {
+        	robot_move sn = gameObject.GetComponent<robot_move>();
+            sn.moveLeft(speed);
+            //pos.x -= panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("m")){
+        	robot_move sn = gameObject.GetComponent<robot_move>();
+        	sn.forceMove();
+        }
+	}
+	// Update is called once per frame
+	void Update () {
+		checkInput();
+	}
+	
+}
---
+++ Simulation/robot_sim/Assets/robot_move.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class robot_move : MonoBehaviour {
+	public Vector3 newLoc;
+    public float speed = 10f;
+    public Transform robot;
+    public float movetime;
+    
+	// Update is called once per frame
+	void Update () {
+        
+	}
+	
+	public void forceMove(){
+
+		
+        StartCoroutine(MoveToPosition(robot, newLoc, movetime));
+
+	}
+	public void moveUp(float upspeed){
+		transform.Translate(new Vector3(0,0,upspeed * Time.deltaTime));
+	}
+	public void moveDown(float downspeed){
+		transform.Translate(new Vector3(0,0,-downspeed * Time.deltaTime));
+	}
+	public void moveLeft(float leftspeed){
+		transform.Translate(new Vector3(-leftspeed * Time.deltaTime,0,0));
+	}
+	public void moveRight(float rightspeed){
+		transform.Translate(new Vector3(rightspeed * Time.deltaTime,0,0));
+	}
+
+	public IEnumerator MoveToPosition(Transform transform, Vector3 position, float timeToMove)
+    {
+      var currentPos = transform.position;
+      var t = 0f;
+       while(t < 1)
+       {
+             t += Time.deltaTime / timeToMove;
+             transform.position = Vector3.Lerp(currentPos, position, t);
+             yield return null;
+      }
+    }
+}
