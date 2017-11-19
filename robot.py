12e33b5663ef5c68983d1be631276b5786502d00
Helena <helenasheinonen@gmail.com>
2017-11-19 18:51:15 -0500

    added catapult

--- robot.py
+++ robot.py
@@ -8,18 +8,25 @@ RIGHT = GenericHID.kRight
 
 class WAPURBot(wpilib.IterativeRobot):
     def robotInit(self):
-        self.l_motor = ctre.CANTalon(3)
-        self.l_motor2 = ctre.CANTalon(4)
         self.r_motor = ctre.CANTalon(1)
         self.r_motor2 = ctre.CANTalon(2)
+        self.l_motor = ctre.CANTalon(3)
+        self.l_motor2 = ctre.CANTalon(4)
         self.robot_drive = wpilib.RobotDrive(self.l_motor, self.l_motor2, self.r_motor, self.r_motor2)
-        self.controller = wpilib.XboxController(0)
+
+        self.catapult_motor = ctre.CANTalon(5)
+
+        self.driver = wpilib.XboxController(0)
+        self.operator = wpilib.XboxController(1)
 
     def teleopPeriodic(self):
-        right_y = self.controller.getY(wpilib.RIGHT)
-        left_x = self.controller.getX(wpilib.LEFT)
+        drive_right_y = self.driver.getY(RIGHT)
+        driver_left_x = self.driver.getX(LEFT)
         self.robot_drive.arcadeDrive(moveValue=right_y, rotateValue=left_x)
+        operator_left_y = self.operator.getY(LEFT)
+        self.catapult_motor.set(operator_left_y)
 
+        
     def autonomousInit(self):
         print("Auto Init")
 
