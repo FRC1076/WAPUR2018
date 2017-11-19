69b1532064d32d93577022f8d0e178d37950eac0
Brian Khuu <2021khuubrianh@aaps.k12.mi.us>
2017-11-19 18:11:55 -0500

    Made accelerometer code
    We need this for the robot to live

--- robot.py
+++ robot.py
@@ -1,26 +1,34 @@
 import wpilib
 import ctre
 
-class WAPURBot(wpilib.IterativeRobot):
-    def robotInit(self):
-        self.l_motor = ctre.CANTalon(3)
-        self.l_motor2 = ctre.CANTalon(4)
-        self.r_motor = ctre.CANTalon(1)
-        self.r_motor2 = ctre.CANTalon(2)
-        self.robot_drive = wpilib.RobotDrive(self.l_motor, self.l_motor2, self.r_motor, self.r_motor2)
-        self.stick = wpilib.Joystick(0)
+class WAPURBot(wpilib.IterativeRobot): 
+    def robotInit(self): 
+        self.l_motor = ctre.CANTalon(3) 
+        self.l_motor2 = ctre.CANTalon(4) 
+        self.r_motor = ctre.CANTalon(1) 
+        self.r_motor2 = ctre.CANTalon(2) 
+        self.robot_drive = wpilib.RobotDrive(self.l_motor, self.l_motor2, self.r_motor, self.r_motor2) 
+        self.stick = wpilib.Joystick(0) 
 
     def teleopPeriodic(self):
         self.robot_drive.arcadeDrive(self.stick)
         print(self.l_motor.get())
+        self.accelerometer = wpilib.BuiltInAccelerometer()
 
-    def autonomousInit(self):
-        print("Auto Init")
+    def autonomousInit(self): 
+        print("Auto Init") 
+ 
+    def autonomousPeriodic(self): 
+        self.l_motor.set(1) 
+        self.r_motor.set(-1)    
 
-    def autonomousPeriodic(self):
-        self.l_motor.set(1)
-        self.r_motor.set(-1)
+    def testPeriodic(self):
+        print("X: {}", self.accelerometer.getX())
+        print("Y: {}", self.accelerometer.getY())
+        print("Z: {}", self.accelerometer.getZ())
 
-if __name__ == '__main__':
+        wpilib.LiveWindow.run()
+
+if __name__ == '__main__': 
     wpilib.run(WAPURBot, physics_enabled=True)
-
+    
