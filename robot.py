2a13cd2fa0e2ddb8a70359795cfad237cb729550
Helena <helenasheinonen@gmail.com>
2017-11-19 18:24:02 -0500

    added arcade drive

--- robot.py
+++ robot.py
@@ -1,6 +1,11 @@
 import wpilib
 import ctre
 
+from wpilib.interfaces import GenericHID
+
+LEFT = GenericHID.kLeft
+RIGHT = GenericHID.kRight
+
 class WAPURBot(wpilib.IterativeRobot):
     def robotInit(self):
         self.l_motor = ctre.CANTalon(3)
@@ -8,11 +13,12 @@ class WAPURBot(wpilib.IterativeRobot):
         self.r_motor = ctre.CANTalon(1)
         self.r_motor2 = ctre.CANTalon(2)
         self.robot_drive = wpilib.RobotDrive(self.l_motor, self.l_motor2, self.r_motor, self.r_motor2)
-        self.stick = wpilib.Joystick(0)
+        self.controller = wpilib.XboxController(0)
 
     def teleopPeriodic(self):
-        self.robot_drive.arcadeDrive(self.stick)
-        print(self.l_motor.get())
+        right_y = self.controller.getY(wpilib.RIGHT)
+        left_x = self.controller.getX(wpilib.LEFT)
+        self.robot_drive.arcadeDrive(moveValue=right_y, rotateValue=left_x)
 
     def autonomousInit(self):
         print("Auto Init")
