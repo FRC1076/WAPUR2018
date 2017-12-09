a3dbe7a5f649aee29467f324ac3da2139ae3a896
Alex Wang <jwang.alex@gmail.com>
2017-12-08 19:26:42 -0500

    Remove useless code in robot.py

--- robot.py
+++ robot.py
@@ -1,45 +1,12 @@
 import wpilib
 import ctre
-import magicbot
 import robotpy_ext
 
 from robotpy_ext.autonomous import AutonomousModeSelector
-from magicbot import StateMachine, state, timed_state
 from wpilib.interfaces import GenericHID
 
 LEFT = GenericHID.Hand.kLeft
 RIGHT = GenericHID.Hand.kRight
-
-class DriveControl(StateMachine):
-    motor = ctre.CANTalon
-
-    def forward(self):
-        self.engage()
-
-    @state(first=True)
-    def prepare_to_forward(self):
-        self.motor.enable()
-
-        if self.motor.is_ready():
-            self.next_state_now('moving')
-
-    @timed_state(duration=1, must_finish=True)
-    def moving(self):
-        self.motor.enable()
-        self.motor()
-
-class DriveTrain:
-    drive_motor = wpilib.RobotDrive
-
-    def __init__(self):
-        self.enabled = False
-
-    def __enable__(self):
-        self.enabled = True
-
-    def forward(self, speed, time):
-        pass
-
 
 class WAPURBot(wpilib.IterativeRobot):
     def robotInit(self):
@@ -67,9 +34,6 @@ class WAPURBot(wpilib.IterativeRobot):
 
     def autonomousPeriodic(self):
         self.automodes.run()
-
-    def forwardCommand(speed, time):
-        speed = self.wheel(1)
 
 if __name__ == '__main__':
     wpilib.run(WAPURBot, physics_enabled=True)
